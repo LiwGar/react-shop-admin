ab41cd0507a031e722f8dd78c8aca77fe12d0bd3
wilgar <wilenri341@hotmail.com>
2023-10-23 22:46:56 -0500

    update de next.config.

--- next.config.js
+++ next.config.js
@@ -3,11 +3,4 @@ module.exports = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
-	swcMinify: true,
-	images: {
-		domains: ['tailwindui.com', 'images.unsplash.com', 'api.escuelajs.co', 'loremflickr.com', 'google.pl',
- 		'placeimg.com','api.lorem.space','vuzoon.com','www.complementosdelcafe.com'],
-	},
-	unoptimized: true,
-	
 };
