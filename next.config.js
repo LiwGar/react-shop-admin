070aa0abeba58b5db57d8cbd622b9f8cb22aa9c1
wilgar <wilenri341@hotmail.com>
2023-10-23 23:36:13 -0500

    update, next_config y package_json.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
 		ignoreDuringBuilds: true,
 	},
 	images: {
-		domains: [ 'tailwindui.com', 'placeimg.com','api.lorem.space','vuzoon.com','www.complementosdelcafe.com'],
+		domains: ['tailwindui.com', 'images.unsplash.com', 'api.escuelajs.co', 'loremflickr.com', 'google.pl',
+ 		'placeimg.com','api.lorem.space','vuzoon.com','www.complementosdelcafe.com'],
 	},
 };
