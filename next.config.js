92ddacfed2a536b0e65635ec1ee6803f1cfb675a
wilgar <wilenri341@hotmail.com>
2023-10-23 22:39:59 -0500

    Adjuste nexconfig.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	reactStrictMode: true,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 	swcMinify: true,
 	images: {
 		domains: ['tailwindui.com', 'images.unsplash.com', 'api.escuelajs.co', 'loremflickr.com', 'google.pl',
  		'placeimg.com','api.lorem.space','vuzoon.com','www.complementosdelcafe.com'],
 	},
 	unoptimized: true,
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+	
 };
